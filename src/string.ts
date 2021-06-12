51de1cc21e1b236156a932e11aa7f22bf3ee97fb
work-work-koba <40133262+work-work-koba@users.noreply.github.com>
2021-06-12 19:35:32 +0900

    Update string.ts

--- src/string.ts
+++ src/string.ts
@@ -1,11 +1,25 @@
 import {applyExtensions} from "./common"
 
-export function applyAllExtensions() {
+export function extendString() {
     applyExtensions(String.prototype, {
         padLeft,
-    })
+        padRight,
+    });
 }
 
 export function padLeft(value, length, padChar=" ") {
-    
+    if (length <= value.length) {
+        return value;
+    }
+
+    return padChar.repeat(length - value.length) + value;
+}
+
+
+export function padRight(value, length, padChar=" ") {
+    if (length <= value.length) {
+        return value;
+    }
+
+    return value + padChar.repeat(length - value.length);
 }
