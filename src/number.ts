d036e4fdc16e58d4097d1422ec71dc2c93b65574
Simpler one (Koba) <40133262+simpler-one@users.noreply.github.com>
2021-06-14 11:00:44 +0900

    Delete number.ts

--- src/number.ts
+++
@@ -1,9 +0,0 @@
-/**
- * 
- * @param value 
- * @param min 
- * @param max 
- */
-export function clip(value: number, min: number, max: number): number {
-    return Math.min(Math.max(min, value), max);
-}
