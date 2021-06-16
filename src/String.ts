733d9e4db60c290beb50393e3f7a1d1d4085437d
Hiroaki Kobayashi <koumei001@gmail.com>
2021-06-17 00:48:58 +0900

    String: +comments

--- src/String.ts
+++ src/String.ts
@@ -1,9 +1,10 @@
 
 /**
- * 
- * @param value 
- * @param length 
- * @param padChar 
+ * Fill left-side of `value` with given `padChar` to `length`.
+ * `value` の左側を `padChar` で `length` の文字列長になるまで埋めます。
+ * @param value text value
+ * @param length target length
+ * @param padChar pad character
  */
 export function padLeft(value: string, length: number, padChar: string=" "): string {
     if (length <= value.length) {
@@ -14,6 +15,13 @@ export function padLeft(value: string, length: number, padChar: string=" "): str
 }
 
 
+/**
+ * Fill right-side of `value` with given `padChar` to `length`.
+ * `value` の右側を `padChar` で `length` の文字列長になるまで埋めます。
+ * @param value text value
+ * @param length target length
+ * @param padChar pad character
+ */
 export function padRight(value: string, length: number, padChar: string=" "): string {
     if (length <= value.length) {
         return value;
