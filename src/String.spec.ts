9523b92a851ae5cc84b75c454bfbef3a4b6de8b7
Simpler one (Koba) <40133262+simpler-one@users.noreply.github.com>
2022-01-07 12:17:36 +0900

    Update String.spec.ts

--- src/String.spec.ts
+++ src/String.spec.ts
@@ -2,52 +2,5 @@ import { padLeft, padRight } from "./String";
 
 
 describe("String", () => {
-    describe("padLeft()", () => {
-        it("should return original value", () => {
-            // Given
-            const value = "abcde";
-            const length = 3;
-            const padChar = " ";
-            // When
-            const result = padLeft(value, length, padChar);
-            // Then
-            expect(result).toEqual(value);
-        });
 
-        it("should return padded value", () => {
-            // Given
-            const value = "abc";
-            const length = 5;
-            const padChar = " ";
-            // When
-            const result = padLeft(value, length, padChar);
-            // Then
-            expect(result).toEqual("  abc");
-        });
-    });
-
-
-    describe("padRight()", () => {
-        it("should return original value", () => {
-            // Given
-            const value = "abcde";
-            const length = 3;
-            const padChar = " ";
-            // When
-            const result = padRight(value, length, padChar);
-            // Then
-            expect(result).toEqual(value);
-        });
-
-        it("should return padded value", () => {
-            // Given
-            const value = "abc";
-            const length = 5;
-            const padChar = " ";
-            // When
-            const result = padRight(value, length, padChar);
-            // Then
-            expect(result).toEqual("abc  ");
-        });
-    });
 });
