ba8b560bde0fb5cd43183904877efae0beb86975
Hiroaki Kobayashi <koumei001@gmail.com>
2022-01-12 00:15:52 +0900

    Date: update format

--- src/Date.ts
+++ src/Date.ts
@@ -1,29 +1,31 @@
 import { fillTemplate } from "./String";
 
-export const WEEKDAY_NAMES = Object.freeze([]);
+export const DEFAULT_WEEKDAY_NAMES = Object.freeze(["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]);
+export const DEFAULT_MONTH_NAMES = Object.freeze(["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]);
 
 
 export function format(
     value: Date, template: string,
-    { monthFormat, weekdayFormat }: {
-        monthFormat?: (month: number) => string,
-        weekdayFormat?: (weekday: number) => string,
+    opt: {
+        monthNames?: string[],
+        weekdayNames?: string[],
     } = undefined
 ): string {
-    const year = `${value.getFullYear()}`;
+    const year = value.getFullYear();
     const month = value.getMonth() + 1;
-    const day = `${value.getDate()}`;
-    const hour = `${value.getHours()}`;
-    const minute = `${value.getMinutes()}`;
-    const second = `${value.getSeconds()}`;
-    const ms = `${value.getMilliseconds()}`;
+    const day = value.getDate();
+    const hour = value.getHours();
+    const minute = value.getMinutes();
+    const second = value.getSeconds();
+    const ms = value.getMilliseconds();
     const wd = value.getDay();
 
     return fillTemplate(template, {
         "{Y}": year,
         "{YYYY}": `${year}`.padStart(4, "0"),
-        "{M}": (monthFormat ?? String)(month),
+        "{M}": month,
         "{MM}": `${month}`.padStart(2, "0"),
+        "{MMM}": (opt?.monthNames ?? DEFAULT_MONTH_NAMES)[month - 1],
         "{D}": `${day}`,
         "{DD}": `${day}`.padStart(2, "0"),
         "{h}": `${hour}`,
@@ -32,8 +34,8 @@ export function format(
         "{mm}": `${minute}`.padStart(2, "0"),
         "{s}": `${second}`,
         "{ss}": `${second}`.padStart(2, "0"),
-        "{ms}": `${minute}`,
+        "{ms}": `${ms}`,
         "{0ms}": `${minute}`.padStart(3, "0"),
-        "{wd}": (weekdayFormat ?? String)(wd),
+        "{wd}": (opt?.weekdayNames ?? DEFAULT_WEEKDAY_NAMES)[wd],
     });
 }
